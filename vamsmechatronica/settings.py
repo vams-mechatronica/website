e12bd1fe5317ec66ef5be794ad696d0c860e4213
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-03-04 23:55:29 +0530

    UPDATE settings

--- vamsmechatronica/settings.py
+++ vamsmechatronica/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@75we+v_yrrolgfk50epw(o=3=szk_)=_j#2q3ujomvp8=(8+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
