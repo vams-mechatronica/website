08e1767612f0a14753ab3c421dcd2db41cccc56f
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-03-07 09:13:19 +0530

    ADD model product

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 class ProductORService(models.Model):
     name = models.CharField(_("Name"), max_length=50)
     description = models.TextField(_("Description"))
+    detail_page_content = QuillField()
     image = models.ImageField(_("Image"), upload_to="productorservice/")
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
