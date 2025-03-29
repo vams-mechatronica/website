4c884277ceeb81332e48af9350541d62f522ac69
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-03-29 12:22:09 +0530

    UPDATE admin models

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, BlogTag, BlogCategories
 
 # Register your models here.
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(BlogCategories)
+class BlogCategoriesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BlogTag)
+class BlogTagAdmin(admin.ModelAdmin):
+    pass
+    
+
+    
+
     
